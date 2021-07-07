36de239b3968a7e220b839575d04e8e85deea2ac
Artyom1771 <a.e.titov2004@gmail.com>
2021-07-08 07:48:57 +0900

    registration added

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render , HttpResponseRedirect
-from users.forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm, UserRegistrationForm
 from django.urls import reverse
 from django.contrib import auth
 
@@ -18,9 +18,18 @@ def login(request):
             print(form.errors)
     else:
         form = UserLoginForm()
-    context = {'title': 'Geekshop-Авторизация','form': form}
+    context = {'title': 'Geekshop-Авторизация', 'form': form}
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm
+
+    context = {'title': 'Geekshop-регистрация', 'form': form}
+    return render(request, 'users/registration.html', context)
